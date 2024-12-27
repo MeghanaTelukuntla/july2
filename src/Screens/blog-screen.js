baf3b06f1a0469fb603ff0ea7f05b8294ba97ef7
MeghanaTelukuntla <meghanatelukuntla40@gmail.com>
2024-12-27 10:25:29 +0530

    Example of Use Reducer

--- src/Screens/blog-screen.js
+++ src/Screens/blog-screen.js
@@ -1,12 +1,14 @@
 // import { Navbar } from "react-bootstrap";
 
 import Navbar from "../navbar/navbars";
+import UseReducer from "../use-reducer/use-reducer";
 
 const BlogScreen=()=>{
     return(
         <>
         <Navbar/>
         <h3>Welcome to Blog Screen</h3>
+        <UseReducer/>
         </>
     );
 };
